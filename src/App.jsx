0c6e9f760b6076dfa2e6aaa2aeca8e55d2ca7f9d
Aayushi Singh <aayushi.singh067@gmail.com>
2025-05-19 19:57:00 +0530

    removed empty css files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
 import ToDo from './components/ToDo.jsx'
+import Login from './components/Login.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <><Navbar/><ToDo/></>
+    },
+    {
+      path: '/login',
+      element: <><Login/></>
+    }
+  ])
 
   return (
     <>
       <div className='container mx-auto my-16 bg-[#4e6760] w-3/4 border-solid border-2 rounded-md border-[#0f0e17] shadow-xl selection:bg-[#be9d63] selection:text-[#454242] min-h-[80vh]'>
-        <Navbar />
-        <ToDo />
+        <RouterProvider router={router}/>
+        {/* <Navbar /> */}
+        {/* <ToDo /> */}
       </div>
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { useState } from 'react'
 
 const Navbar = () => {
     const [date, setDate] = useState(new Date())
 
     return (
-        <nav 
-        className='flex items-center flex-nowrap text-[#fffffe] border-b-solid border-b-2 rounded-sm border-[#0f0e17] bg-[#05281c]'>
-            <ul className='flex items-center w-full justify-between p-2'>
-                <li className='text-3xl font-bold'>TO-DAY</li>
-                <li className='text-2xl font-bold'>{date.toDateString()}</li>
-            </ul>
-        </nav>
+        <>
+            <Link to={'/login'}>
+                <button className="mx-2">
+                    Login
+                </button>
+            </Link>
+            <nav
+                className='flex items-center flex-nowrap text-[#fffffe] border-b-solid border-b-2 rounded-sm border-[#0f0e17] bg-[#05281c]'>
+                <ul className='flex items-center w-full justify-between p-2'>
+                    <li className='text-3xl font-bold'>TO-DAY</li>
+                    <li className='text-2xl font-bold'>{date.toDateString()}</li>
+                </ul>
+            </nav>
+        </>
     )
 }
 
