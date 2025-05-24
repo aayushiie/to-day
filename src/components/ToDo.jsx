d4ea128f44ae1c8646b4d7f7438e8cb84eb239bf
Aayushi Singh <aayushi.singh067@gmail.com>
2025-05-24 06:24:04 +0530

    updated mobile ui

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -70,7 +70,7 @@ const AddToDo = () => {
     <>
       <div className="border-solid border-2 rounded-md border-[#0f0e17] my-3 mx-6 shadow-lg text-[#fffffe] p-4">
         <h2 className="pb-2 pt-4 px-2 underline text-lg">Add your task</h2>
-        <div className="add-task-container">
+        <div className="add-task-container flex gap-2">
           <input
             onChange={handleChange}
             value={todo}
@@ -78,7 +78,7 @@ const AddToDo = () => {
             onKeyDown={(e) => {
               if (e.key === 'Enter') handleAdd();
             }}
-            className="task-input"
+            className="task-input flex-grow px-2 py-1 text-black rounded-md"
           />
           <button onClick={handleAdd} className="add-btn flex items-center justify-center">
             <lord-icon
@@ -113,49 +113,56 @@ const AddToDo = () => {
                 <Reorder.Item
                   key={item.id}
                   value={item}
-                  className="flex flex-col sm:flex-row px-4 my-2 items-start sm:items-end text-lg w-full sm:justify-between"
+                  className="flex px-4 my-2 text-lg w-full"
                 >
-                  <div className='flex items-center justify-between border w-full rounded-lg px-2 py-2 bg-white shadow-[0px 0px 0px rgba(0,0,0,0.8)] sm:flex-row'>
-                  <div className="flex items-center">
-                    <input
-                      name={item.id}
-                      className="accent-[#253a40] w-5 h-5"
-                      onChange={handleCheckbox}
-                      type="checkbox"
-                      checked={item.isCompleted}
-                    />
-                  </div>
-                  <div
-                    className={
-                      item.isCompleted
-                        ? 'line-through w-full mx-2 text-black sm:mt-0 text-xl'
-                        : 'w-full mx-2 text-black sm:mt-0 text-xl'
-                    }
-                  >
-                    {item.todo}
-                  </div>
-                  <div className="buttons my-2 flex">
-                    <button
-                      onClick={(e) => handleEdit(e, item.id)}
-                      className="add-btn mx-2 flex justify-center items-center todo-btn"
-                    >
-                      <lord-icon
-                        src="https://cdn.lordicon.com/mudwpdhy.json"
-                        trigger="hover"
-                        colors="primary:#ffffff"
-                      ></lord-icon>
-                    </button>
-                    <button
-                      onClick={(e) => handleDelete(e, item.id)}
-                      className="add-btn mx-2 flex justify-center items-center todo-btn"
-                    >
-                      <lord-icon
-                        src="https://cdn.lordicon.com/oqeixref.json"
-                        trigger="hover"
-                        colors="primary:#ffffff"
-                      ></lord-icon>
-                    </button>
-                  </div>
+                  <div className="flex flex-col sm:flex-row sm:items-center justify-between border w-full rounded-lg px-2 py-3 bg-white shadow-md gap-3">
+                    
+                    {/* Checkbox + Text */}
+                    <div className="flex items-start sm:items-center w-full">
+                      <input
+                        name={item.id}
+                        className="accent-[#253a40] w-5 h-5 mt-1 sm:mt-0 flex-shrink-0"
+                        onChange={handleCheckbox}
+                        type="checkbox"
+                        checked={item.isCompleted}
+                      />
+                      <div
+                        className={`ml-3 text-black text-base sm:text-lg ${
+                          item.isCompleted ? 'line-through' : ''
+                        } break-words`}
+                        style={{
+                          wordBreak: 'break-word',
+                          overflowWrap: 'break-word',
+                          maxWidth: '100%',
+                        }}
+                      >
+                        {item.todo}
+                      </div>
+                    </div>
+
+                    {/* Buttons */}
+                    <div className="flex gap-3 justify-end sm:justify-center items-center flex-shrink-0">
+                      <button
+                        onClick={(e) => handleEdit(e, item.id)}
+                        className="add-btn flex justify-center items-center todo-btn"
+                      >
+                        <lord-icon
+                          src="https://cdn.lordicon.com/mudwpdhy.json"
+                          trigger="hover"
+                          colors="primary:#ffffff"
+                        ></lord-icon>
+                      </button>
+                      <button
+                        onClick={(e) => handleDelete(e, item.id)}
+                        className="add-btn flex justify-center items-center todo-btn"
+                      >
+                        <lord-icon
+                          src="https://cdn.lordicon.com/oqeixref.json"
+                          trigger="hover"
+                          colors="primary:#ffffff"
+                        ></lord-icon>
+                      </button>
+                    </div>
                   </div>
                 </Reorder.Item>
               ))}
@@ -163,7 +170,7 @@ const AddToDo = () => {
         </div>
       </div>
 
-      <div className="my-3 mx-6 p-4 flex flex-col sm:flex-row justify-between">
+      <div className="my-3 mx-6 p-4 flex flex-col sm:flex-row justify-between gap-2">
         <button
           onClick={handleCompletedTasks}
           className="add-btn mx-2 flex justify-center items-center gap-2"
